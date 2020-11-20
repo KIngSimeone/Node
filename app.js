40bf14b1faecbf06261f40c9a65d216019603424
Kingsimeone <simeoneumoh@gmail.com>
2020-11-20 22:00:49 +0100

    App.JS Setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path');
+
+var app = express();
+
+
+app.use(function(req, res, next){
+    console.log('Time: ', Date.now());
+    next;
+})
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port);
+console.log('Server Listening on port '+port);
+
+module.exports = app;
